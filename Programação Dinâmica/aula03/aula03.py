f2b4d49da763f5bfd0dbd354b7bae89f233d5566
Guilherme Barone Milani <guibarone.milani@gmail.com>
2026-03-24 11:43:40 -0300

    Update aula03.py

--- Programação Dinâmica/aula03/aula03.py
+++ Programação Dinâmica/aula03/aula03.py
@@ -14,6 +14,7 @@ def inverte_string(texto):
 
 print(inverte_string('palavra'))
 
+
 # 2
 def inverte_texto(texto):
     lista = []
@@ -38,4 +39,73 @@ def inverte_texto(texto):
 
 inverte_texto('Amor a roma')
 
+
 # 3
+def verificar_comandos(lista):
+    resultado = []
+
+    for comando in lista:
+        if comando == "UNDO":
+            if len(resultado) > 0:
+                resultado.pop()
+        else:
+            resultado.append(comando)
+
+    return resultado
+
+entrada = ["A", "B", "C", "UNDO", "D"]
+print(verificar_comandos(entrada))
+
+
+# 4
+def remove_pares_iguais(texto)
+    lista = []
+    for char in texto:
+        if len(lista) > 0 and lista[-1] == char:
+            pilha.pop()
+        else:
+            pilha.append(char)
+
+entrada = 'abbaca'
+print(remove_pares_iguais(entrada))
+
+
+# 5
+def verificar_parenteses_balanceados():
+    lista = []
+    for char in expressao:
+        if char == "(":
+            lista.append(char)
+        elif char == ")":
+            if len(lista) == 0:
+                return False
+            lista.pop()
+
+    return len(lista) == 0
+
+expr = input("Digite a expressão: ")
+
+if verificar_parenteses_balanceados(expr):
+    print("Parênteses balanceados ✔")
+else:
+    print("Parênteses NÃO balanceados ❌")          
+
+
+# 6
+def decimal_para_binario(numero):
+    lista = []
+
+    while numero > 0:
+        resto = numero % 2
+        lista.append(resto)
+        numero = numero // 2
+
+    binario = ''
+
+    while len(lista) > 0:
+        binario += str(lista.pop())
+
+    return binario
+
+num = int(input('Digite um número decimal: '))
+print(f'Binário de {num}', decimal_para_binario(num))
